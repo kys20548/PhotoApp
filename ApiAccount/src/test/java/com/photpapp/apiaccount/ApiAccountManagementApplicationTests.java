14f40d475c1c9d6f443ea65ce13d8a430a79cfd2
danny.kao <danny.kao@leadtek.io>
2023-06-20 17:59:31 +0800

    init

---
+++ ApiAccount/src/test/java/com/photpapp/apiaccount/ApiAccountManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.photpapp.apiaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAccountManagementApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

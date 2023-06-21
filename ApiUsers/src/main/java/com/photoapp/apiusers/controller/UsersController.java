ef9e9dc014c7f9c6c4dd4ec596fb7bd6d455e836
danny.kao <danny.kao@leadtek.io>
2023-06-21 11:27:43 +0800

    add 負載平衡,啟動多個instance,h2-database

--- ApiUsers/src/main/java/com/photoapp/apiusers/controller/UsersController.java
+++ ApiUsers/src/main/java/com/photoapp/apiusers/controller/UsersController.java
@@ -1,5 +1,6 @@
 package com.photoapp.apiusers.controller;
 
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UsersController {
 
+  private final Environment env;
+
+  public UsersController(Environment env) {
+    this.env = env;
+  }
+
   @GetMapping("/check")
   public String check(){
-    return "alive";
+    return "Service alive,port number : "+env.getProperty("local.server.port");
   }
 
 }
